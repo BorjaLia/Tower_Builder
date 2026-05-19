c9e3f8ac53b7bdaa59081091fab952279307eb2e
Borja <borja.lia.deichler@gmail.com>
2026-05-18 23:45:09 -0300

    Add music and bg to main screen

--- Assets/Scrips/Ui/SettingsManager.cs
+++ Assets/Scrips/Ui/SettingsManager.cs
@@ -17,6 +17,11 @@ public class SettingsManager : MonoBehaviour
             musicSlider.value = DataManager.s_instance.musicVolume;
             sfxSlider.value = DataManager.s_instance.sfxVolume;
             uiSlider.value = DataManager.s_instance.uiVolume;
+
+            AudioManager.s_instance.SetVolume("MasterVolume", masterSlider.value);
+            AudioManager.s_instance.SetVolume("MusicVolume", musicSlider.value);
+            AudioManager.s_instance.SetVolume("SFXVolume", sfxSlider.value);
+            AudioManager.s_instance.SetVolume("UIVolume", uiSlider.value);
         }
 
         masterSlider.onValueChanged.AddListener(SetMasterVolume);
--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 A simple 3D tower stacking game
 
+## Itch Page
+
+- [Game Page](https://borjaliad.itch.io/tower-builder)
+
 ## Authors
 
 - [@BorjaLia](https://www.github.com/BorjaLia)
